7f0fa48d8c84a3fb7d9a4813c941bb1df8010903
María Serna Ruiz <147938132+mariasr79@users.noreply.github.com>
2024-07-17 18:30:07 +0000

    navbar mejorado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,21 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import logostarwars from "../../img/star wars1.png";
+import "./../../styles/navbar.css";
+
 export const Navbar = () => {
 	const {store,actions} = useContext(Context);
 	return (
 		<div>
 			<nav className="navbar navbar-light bg-light mb-3 ">
+            <Link to="/">
+				<span className="navbar-brand mb-0 h1"><img className="imagenDetalle" src={logostarwars}></img></span>
+			</Link>
+                
             <div>
                 <div className="btn-group botonFav">
-                    <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         Favoritos {store.fav.length}
                     </button>
                     <ul className="dropdown-menu">
